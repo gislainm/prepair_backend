7cacb5fd1bb3cdc3eb4ee599dc50d33a8ed0f83b
gislainm <gmuhikira2@gmail.com>
2023-01-26 14:01:17 -0600

    updating app file

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use((err, req, res, next) => {
 mongoose.set("strictQuery", false);
 mongoose.connect(process.env.mongoAtlasUri)
     .then(() => {
-        app.listen(process.env.port, () => { console.log('welcome to Pre-Pair') })
+        app.listen(process.env.port  || 8080, () => { console.log('welcome to Pre-Pair') })
     })
     .catch((error)=>{
         console.log(error)
