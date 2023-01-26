c00eaad5e3392469fcc1dc5cb5195cdb68f52e18
gislainm <gmuhikira2@gmail.com>
2023-01-26 16:26:54 -0600

    fixed getMentor bug

--- controllers/userController.js
+++ controllers/userController.js
@@ -87,52 +87,10 @@ exports.getMentors = async (req, res, next) => {
         const mentors = await User.find({ Role: 'Mentor', Discipline: discipline });
         res.status(200).json(new responseInfo(false, null, mentors));
     } catch (error) {
+        console.log(error)
         res.status(400).json(new responseInfo(true, "searching mentors failed", null));
     }
 }
-
-// exports.authenticate = (req, res, next) => {
-//     const [, token] = req.headers.authorization.split(" ");
-//     try {
-//         let permission = jwt.verify(token, SECRET);
-//         res.status(200).json(new responseInfo(false, null, permission));
-//     } catch (err) {
-//         res.status(400).json(new responseInfo(true, "Invalid JWT", null));
-//     }
-// }
-
-// exports.fetchProfile = async (req, res, next) => {
-//     const email = req.params.email;
-//     try {
-//         const userInfo = await User.findOne({ email });
-//         res.status(200).json(new responseInfo(false, null, userInfo));
-//     } catch (error) {
-//         res.status(400).json(new responseInfo(true, "profile fetch failed", null));
-//     }
-// }
-
-// exports.completeUserInfo = async (req, res, next) => {
-//     const email = req.body.email;
-//     const newUserInfo = req.body.moreInfo
-//     if (newUserInfo.Gender === "Male") {
-//         newUserInfo.Image = "user1.png";
-//     } else if (newUserInfo.Gender === "Female") {
-//         newUserInfo.Image = "FemaleUser.png";
-//     }
-//     try {
-//         const updateUser = await User.updateOne({ email }, newUserInfo);
-//         const updatedUser = await User.findOne({ email });
-//         const accessToken = jwt.sign({
-//             id: updatedUser._id,
-//             email: updatedUser.email,
-//             iat: Date.now()
-//         }, SECRET);
-//         res.status(200).json(new responseInfo(false, null, { accessToken, updatedUser }));
-//     } catch (error) {
-//         res.status(400).json(new responseInfo(true, "completing user's information failed", null))
-//     }
-// }
-
 
 exports.getAllUser = async(req,res,next)=>{
     try {
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -4,14 +4,11 @@ const userController = require('../controllers/userController');
 const express = require('express');
 const router = express.Router();
 
-// router.get('/findMentor', userController.findMentor);
 router.get('/getMentors/:discipline', userController.getMentors);
 router.post('/login', userController.login);
 router.post('/signup', userController.signup);
 router.put('/updateUserInfo',userController.updateUserInfo);
 router.put('/updateUserPwd',userController.updateUserPwd);
 router.get('/getAll',userController.getAllUser);
-// router.post('/complete', userController.completeUserInfo);
-// router.get('/user/:email', userController.fetchProfile);
 
 module.exports = router;
